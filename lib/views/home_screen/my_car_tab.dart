03cf4e6bd11b2b51808ee697c02c7c0f391c696e
Chamath <chamath@flexiata.lk>
2023-12-24 19:46:59 +0530

    Create my_car_tab.dart

---
+++ lib/views/home_screen/my_car_tab.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MyCarTab extends StatefulWidget {
+  const MyCarTab({super.key});
+
+  @override
+  State<MyCarTab> createState() => _MyCarTabState();
+}
+
+class _MyCarTabState extends State<MyCarTab> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Column(
+      children: [],
+    ));
+  }
+}
