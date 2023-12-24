502bc2fcfbf11eed3d827cdd04aafeb4bb05da80
Chamath <chamath@flexiata.lk>
2023-12-24 15:18:47 +0530

    Update home_page.dart

--- lib/views/home_screen/home_page.dart
+++ lib/views/home_screen/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:taxia/constants/app_colors.dart';
 import 'package:taxia/providers/user/user_provider.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,6 +12,22 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int currentIndex = 0;
+
+  final List<Widget> pages = [
+    Center(
+      child: Text("Home"),
+    ),
+    Center(
+      child: Text("Business"),
+    ),
+    Center(
+      child: Text("Notifications"),
+    ),
+    Center(
+      child: Text("My Info"),
+    ),
+  ];
   @override
   void initState() {
     super.initState();
@@ -53,24 +70,74 @@ class _HomePageState extends State<HomePage> {
             child: Icon(Icons.logout),
           ),
         ),
-        body: TabBarView(
-          children: [
-            Container(
-              child: Center(
-                child: Text("Home"),
+        body: IndexedStack(
+          index: currentIndex,
+          children: pages,
+        ),
+        // TabBarView(
+        //   children: [
+        //     Container(
+        //       child: Center(
+        //         child: Text("Home"),
+        //       ),
+        //     ),
+        //     Container(
+        //       child: Center(
+        //         child: Text("My Car"),
+        //       ),
+        //     ),
+        //     Container(
+        //       child: Center(
+        //         child: Text("Travel"),
+        //       ),
+        //     ),
+        //   ],
+        // ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: currentIndex,
+          onTap: (index) {
+            setState(() {
+              currentIndex = index;
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home_outlined,
+                color: AppColors.iconColor,
               ),
+              label: "Home",
             ),
-            Container(
-              child: Center(
-                child: Text("My Car"),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.badge_outlined,
+                color: AppColors.iconColor,
               ),
+              label: "Business",
             ),
-            Container(
-              child: Center(
-                child: Text("Travel"),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.notifications_none_outlined,
+                color: AppColors.iconColor,
               ),
+              label: "Notification",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.person_2_outlined,
+                color: AppColors.iconColor,
+              ),
+              label: "My Info",
             ),
           ],
+          showSelectedLabels: true,
+          showUnselectedLabels: true,
+          type: BottomNavigationBarType.fixed,
+          selectedLabelStyle: TextStyle(
+              color: AppColors.textColor, fontWeight: FontWeight.w500),
+          unselectedLabelStyle: TextStyle(
+            color: AppColors.textColor,
+          ),
         ),
       ),
     );
