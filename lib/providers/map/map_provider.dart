4a05f76aaccca11b38adfaf85f1b0398c9eb407c
Chamath <chamath@flexiata.lk>
2023-12-28 12:45:43 +0530

    Map Completed

---
+++ lib/providers/map/map_provider.dart
@@ -0,0 +1,91 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapProvider extends ChangeNotifier {
+  final Completer<GoogleMapController> googleMapCompleterController =
+      Completer<GoogleMapController>();
+
+  LatLng? pickupLocation;
+  LatLng? endLocation;
+
+  List<LatLng> polylineCordinates = [];
+
+  Position? currentPositionOfUser;
+
+  //final Set<Polyline> _polyline = {};
+  Set<Marker> markers = Set<Marker>();
+
+  GoogleMapController? controllerGoogleMap;
+
+  final TextEditingController pickupLocationController =
+      TextEditingController();
+  final TextEditingController endLocationController = TextEditingController();
+
+  void drowPolyline(String placeID) async {
+    PolylinePoints polylinePoints = PolylinePoints();
+
+    PolylineResult polylineResult =
+        await polylinePoints.getRouteBetweenCoordinates(
+      "AIzaSyDWlxEQU9GMmFEmZwiT3OGVVxTyc984iNY",
+      PointLatLng(pickupLocation!.latitude, pickupLocation!.longitude),
+      PointLatLng(endLocation!.latitude, endLocation!.longitude),
+    );
+
+    polylineCordinates.clear();
+
+    polylineResult.points.forEach(
+      (PointLatLng points) => polylineCordinates.add(
+        LatLng(points.latitude, points.longitude),
+      ),
+    );
+    notifyListeners();
+  }
+
+  gerCurrentLiveLocationOfUser() async {
+    Position positionOfUser = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.bestForNavigation);
+
+    currentPositionOfUser = positionOfUser;
+
+    LatLng positionOfUserInLatLng = LatLng(
+        currentPositionOfUser!.latitude, currentPositionOfUser!.longitude);
+
+    CameraPosition cameraPosition =
+        CameraPosition(target: positionOfUserInLatLng, zoom: 15);
+
+    controllerGoogleMap!
+        .animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+    notifyListeners();
+  }
+
+  void focusCameraOnPickupAndEndLocations() {
+    if (pickupLocation != null && endLocation != null) {
+      LatLngBounds bounds = LatLngBounds(
+        southwest: LatLng(
+          pickupLocation!.latitude < endLocation!.latitude
+              ? pickupLocation!.latitude
+              : endLocation!.latitude,
+          pickupLocation!.longitude < endLocation!.longitude
+              ? pickupLocation!.longitude
+              : endLocation!.longitude,
+        ),
+        northeast: LatLng(
+          pickupLocation!.latitude > endLocation!.latitude
+              ? pickupLocation!.latitude
+              : endLocation!.latitude,
+          pickupLocation!.longitude > endLocation!.longitude
+              ? pickupLocation!.longitude
+              : endLocation!.longitude,
+        ),
+      );
+
+      controllerGoogleMap!.animateCamera(
+        CameraUpdate.newLatLngBounds(bounds, 50.0),
+      );
+    }
+  }
+}
--- lib/views/map/map_page.dart
+++ lib/views/map/map_page.dart
@@ -1,9 +1,10 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:geocoding/geocoding.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:taxia/widgets/google_autocomplete_textFiled.dart';
+import 'package:google_places_flutter/google_places_flutter.dart';
+import 'package:google_places_flutter/model/prediction.dart';
+import 'package:provider/provider.dart';
+import 'package:taxia/global/global.dart';
+import 'package:taxia/providers/map/map_provider.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -13,64 +14,186 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
-  String? _mapStyle;
-
-  late LatLng pickup;
-  late LatLng end;
-
-  Set<Marker> markers = Set<Marker>();
-
-  GoogleMapController? mapController;
-
-  final TextEditingController pickupLocationController =
-      TextEditingController();
-  final TextEditingController endLocationController = TextEditingController();
-
-  static const LatLng _pGooglePlex = LatLng(37.4223, -122.0848);
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          GoogleMap(
-            markers: markers,
-            onMapCreated: (GoogleMapController controller) {
-              mapController = controller;
-              mapController!.setMapStyle(_mapStyle);
-            },
-            initialCameraPosition: CameraPosition(
-              target: _pGooglePlex,
-              zoom: 13,
-            ),
-          ),
-          inputFiledsContainer(),
-        ],
-      ),
-    );
-  }
-
-  Widget inputFiledsContainer() {
-    return Positioned(
-      top: 30,
-      left: 20,
-      right: 20,
-      child: Container(
-        child: Column(
+    return Consumer(
+      builder: (BuildContext context, MapProvider mapProvider, Widget? child) =>
+          Scaffold(
+        body: Stack(
           children: [
-            GoogleAutoCompleteTextFiled(
-              controller: pickupLocationController,
-              hintText: 'Pickup Location',
-              onPlaceSelected: (place) {
-                //_handlePickupLocationSelected(place);
+            GoogleMap(
+              markers: mapProvider.markers,
+              myLocationEnabled: true,
+              zoomControlsEnabled: false,
+              polylines: {
+                Polyline(
+                  polylineId: PolylineId("route"),
+                  points: mapProvider.polylineCordinates,
+                  visible: true,
+                  width: 5,
+                  color: Colors.green,
+                ),
               },
+              onMapCreated: (GoogleMapController mapController) {
+                mapProvider.controllerGoogleMap = mapController;
+
+                mapProvider.googleMapCompleterController
+                    .complete(mapProvider.controllerGoogleMap);
+              },
+              initialCameraPosition: googlePlexInitialPosition,
             ),
-            GoogleAutoCompleteTextFiled(
-              controller: endLocationController,
-              hintText: 'Where are you going?',
-              onPlaceSelected: (place) {
-                //_handleEndLocationSelected(place);
-              },
+            Positioned(
+              top: 30,
+              left: 20,
+              right: 20,
+              child: Container(
+                child: Column(
+                  children: [
+                    GooglePlaceAutoCompleteTextField(
+                      boxDecoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10.0),
+                        border: Border.all(
+                          color: Colors.blueAccent,
+                        ),
+                      ),
+                      textEditingController:
+                          mapProvider.pickupLocationController,
+                      googleAPIKey: "AIzaSyDWlxEQU9GMmFEmZwiT3OGVVxTyc984iNY",
+                      inputDecoration: InputDecoration(
+                        hintText: "Pickup",
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          borderSide: BorderSide.none,
+                        ),
+                        contentPadding: const EdgeInsets.symmetric(
+                            vertical: 0, horizontal: 10),
+                      ),
+                      debounceTime: 800,
+                      isLatLngRequired: true,
+                      getPlaceDetailWithLatLng: (Prediction prediction) {
+                        mapProvider.pickupLocation = LatLng(
+                          double.parse(prediction.lat.toString()),
+                          double.parse(prediction.lng.toString()),
+                        );
+
+                        mapProvider.markers.add(
+                          Marker(
+                            icon: BitmapDescriptor.defaultMarker,
+                            markerId: MarkerId('start'),
+                            position: mapProvider.pickupLocation!,
+                            infoWindow: InfoWindow(title: 'Start Location'),
+                          ),
+                        );
+
+                        // CameraPosition cameraPosition =
+                        //     CameraPosition(target: pickupLocation!, zoom: 15);
+
+                        // controllerGoogleMap!.animateCamera(
+                        //     CameraUpdate.newCameraPosition(cameraPosition));
+                        //print(pickupLocation);
+                      },
+                      itemClick: (Prediction prediction) {
+                        mapProvider.pickupLocationController.text =
+                            prediction.description!;
+                        mapProvider.pickupLocationController.selection =
+                            TextSelection.fromPosition(
+                          TextPosition(offset: prediction.description!.length),
+                        );
+                      },
+                      itemBuilder: (context, index, Prediction prediction) {
+                        return Container(
+                          padding: EdgeInsets.all(10),
+                          child: Row(
+                            children: [
+                              Icon(Icons.location_on),
+                              SizedBox(
+                                width: 7,
+                              ),
+                              Expanded(
+                                child: Text("${prediction.description ?? ""}"),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                      isCrossBtnShown: true,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    GooglePlaceAutoCompleteTextField(
+                      boxDecoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10.0),
+                        border: Border.all(
+                          color: Colors.blueAccent,
+                        ),
+                      ),
+                      textEditingController: mapProvider.endLocationController,
+                      googleAPIKey: "AIzaSyDWlxEQU9GMmFEmZwiT3OGVVxTyc984iNY",
+                      inputDecoration: InputDecoration(
+                        hintText: "End Location",
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          borderSide: BorderSide.none,
+                        ),
+                        contentPadding: const EdgeInsets.symmetric(
+                            vertical: 0, horizontal: 10),
+                      ),
+                      debounceTime: 800,
+                      isLatLngRequired: true,
+                      getPlaceDetailWithLatLng: (Prediction prediction) {
+                        mapProvider.endLocation = LatLng(
+                          double.parse(prediction.lat.toString()),
+                          double.parse(prediction.lng.toString()),
+                        );
+
+                        mapProvider.markers.add(
+                          Marker(
+                            icon: BitmapDescriptor.defaultMarker,
+                            markerId: MarkerId('end'),
+                            position: mapProvider.endLocation!,
+                            infoWindow: InfoWindow(title: 'End Location'),
+                          ),
+                        );
+
+                        // Call the drawPolyline method
+                        mapProvider.drowPolyline('start-end');
+
+                        mapProvider.focusCameraOnPickupAndEndLocations();
+
+                        //print(endLocation);
+                      },
+                      itemClick: (Prediction prediction) {
+                        mapProvider.endLocationController.text =
+                            prediction.description!;
+                        mapProvider.endLocationController.selection =
+                            TextSelection.fromPosition(
+                          TextPosition(offset: prediction.description!.length),
+                        );
+                      },
+                      itemBuilder: (context, index, Prediction prediction) {
+                        return Container(
+                          padding: EdgeInsets.all(10),
+                          child: Row(
+                            children: [
+                              Icon(Icons.location_on),
+                              SizedBox(
+                                width: 7,
+                              ),
+                              Expanded(
+                                  child:
+                                      Text("${prediction.description ?? ""}")),
+                            ],
+                          ),
+                        );
+                      },
+                      isCrossBtnShown: true,
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
